34a6cfe2dbab780ebedd674c41578adbf46ffbe3
softlang-net <x915927@icloud.com>
2024-01-15 22:27:56 +0800

    pgpool and run

--- database/postgresql/pgpool2/zybpool/workdir/run.zsh
+++ database/postgresql/pgpool2/zybpool/workdir/run.zsh
@@ -2,8 +2,8 @@ set -e
 
 workdir=$(realpath $(dirname $0))
 echo "workdir = $workdir"
-rm -f $workdir/socks/.s.*
-pgpool -f $workdir/pgpool.conf -n -D # & disown %-  #-d
+rm -f $workdir/socks/.s.* && sleep 1
+exec pgpool -f $workdir/pgpool.conf -n -D # -m fast & disown %-  #-d
 
 # run by root>
 #chown -R postgres:postgres $workdir 
