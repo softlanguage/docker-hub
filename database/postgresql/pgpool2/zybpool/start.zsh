7aca6c04fa0dcf157353ae5fb067c17ea1c53d9b
softlang-net <x915927@icloud.com>
2024-01-16 16:38:17 +0800

    pgpool

--- database/postgresql/pgpool2/zybpool/start.zsh
+++ database/postgresql/pgpool2/zybpool/start.zsh
@@ -6,23 +6,30 @@ echo "workdir = $workdir"
 mkdir -p $workdir/workdir/socks
 chown -R 26:26 $workdir/workdir
 
-docker compose -p pgpool2 -f $workdir/compose.yaml up -d --remove-orphans
-docker ps -l
+name4pod=ii-pgpool2
 
-echo 'well done.'
-exit 0
+if command -v docker > /dev/null; then
+    docker compose -p $name4pod -f $workdir/compose.yaml up -d --remove-orphans
+    docker ps -l
+    echo 'well done by docker.'
+else
+    podman run -itd --name $name4pod -h $name4pod --cpus '1' -m 1g \
+	    --tz Asia/Shanghai --network host --restart always --init \
+	    -e PGHOST=/workdir/socks -e PGPORT=5678 \
+	    --tmpfs /dev/shm --no-healthcheck \
+	    -v $workdir/workdir/:/workdir \
+	    docker.io/softlang/pgpool:v4.4.3 /bin/sh -c '
+set -e
+grep "postgres" /etc/passwd
+if command -v /workdir/run.zsh > /dev/null; then
+    exec /workdir/run.zsh
+fi
+echo "Error: no /workdir/run.zsh"
+exec tail -f /dev/null'
 
-#mkdir -p $workdir/{data,backup}
-#chown -R 999:999 $workdir/{data,backup}
-#mybox=pgdb13zyb
-#img_tag=docker.io/softlang/citus-cn:11.3-pg13fdw
-#podman rm -i -t 15 -f $mybox
-#podman run -itd --name $mybox -h $mybox --cpus '2' -m 2g \
-#	--tz Asia/Shanghai --network host --restart always --init \
-#	-e POSTGRES_PASSWORD=123654 -e PGPORT=5430 \
-#	--tmpfs /dev/shm --no-healthcheck \
-#	-v $workdir/data:/var/lib/postgresql/data \
-#	$img_tag bash
+    podman ps -l
+    echo 'well done by podman.'
+fi
+
 # pg_basebackup --progress -X stream -U postgres  -h 10.8.8.204 -p 5432 -R -D ./ & disown %-
-podman ps -l
 # podman run -itd --name oraclelinux -h oraclelinux -m 2G --init oraclelinux:9
--- database/postgresql/pgpool2/zybpool/workdir/run.zsh
+++ database/postgresql/pgpool2/zybpool/workdir/run.zsh
@@ -3,7 +3,9 @@ set -e
 workdir=$(realpath $(dirname $0))
 echo "workdir = $workdir"
 rm -f $workdir/socks/.s.* && sleep 1
-exec pgpool -f $workdir/pgpool.conf -n -D # -m fast & disown %-  #-d
+exec pgpool -f $workdir/pgpool.conf -m fast -n -D # -m fast & disown %-  #-d
+# reload pool_passwd
+# pgpool -f /workdir/pgpool.conf reload
 
 # run by root>
 #chown -R postgres:postgres $workdir 
