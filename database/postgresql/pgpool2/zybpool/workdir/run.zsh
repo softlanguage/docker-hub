f702f74c065466c7da1bdb7e10dcda3e0efa366a
softlang-net <x915927@icloud.com>
2024-01-16 21:54:27 +0800

    pgpool test done

--- database/postgresql/pgpool2/zybpool/workdir/run.zsh
+++ database/postgresql/pgpool2/zybpool/workdir/run.zsh
@@ -2,8 +2,8 @@ set -e
 
 workdir=$(realpath $(dirname $0))
 echo "workdir = $workdir"
-rm -f $workdir/socks/.s.* && sleep 1
-exec pgpool -f $workdir/pgpool.conf -m fast -n -D # -m fast & disown %-  #-d
+pkill pgpool && sleep 1
+exec pgpool -f $workdir/pgpool.conf -m fast -n -D # & disown %-  #-d
 # reload pool_passwd
 # pgpool -f /workdir/pgpool.conf reload
 
