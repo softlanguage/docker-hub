ebbc5d162dfbdb0eb90752fde7a4dae4ee97f8d6
softlang-net <bug.fyi@outlook.com>
2024-04-23 20:34:39 +0800

    update docker-cli to 25.0.5 on Jenkins

--- devops/jenkins/Dockerfile.py
+++ devops/jenkins/Dockerfile.py
@@ -3,14 +3,13 @@
 FROM jenkins/jenkins:2.414.3-lts-alpine
 
 USER root
-# install build tools
+# install build tools (podman-remote)
 RUN sed -i s/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g /etc/apk/repositories \
-    && apk update && apk add --no-cache tzdata docker-cli podman-remote python3 \
+    && apk update && apk add --no-cache tzdata docker-cli python3 \
     && rm -rf /var/cache/apk/*
 
 USER jenkins
-RUN pip config set global.index-url http://mirrors.aliyun.com/pypi/simple/ \
-    && pip config set global.trusted-host mirrors.aliyun.com 
+RUN python -m ensurepip --upgrade
 ENV TZ=Asia/Shanghai
 ENV JAVA_OPTS=-Xmx128m 
 WORKDIR /tmp
