f0453bdcf7e54447fdf5535e0a8762032f63f898
softlang-net <bug.fyi@outlook.com>
2024-09-05 22:05:49 +0800

    --memory-swap 0 , memswap_limit: 0

--- database/greenplum7/start.sh
+++ database/greenplum7/start.sh
@@ -8,9 +8,10 @@ docker run -d --name gpdb7rhel8.local -h gpdb7rhel8.local -e TZ=Asia/Shanghai \
     --add-host "gp7m1.local:127.0.0.1" \
     --add-host "gp7s1.local:127.0.0.1" \
     --add-host "gp7s2.local:127.0.0.1" \
-    --shm-size 1gb -m 4gb --memory-swap 4gb --kernel-memory 1gb \
+    --shm-size 1gb -m 4gb --memory-swap 0 \
     --cpuset-cpus 0,1 \
     localhost/gpdb7:rhel8
 
+# --memory-swap 0, will use less memeory as buff/cache
 # --kernel-memory 1gb, do not effective of buff/cache
 # --shm-size 1gb -m 4gb --memory-swap 4gb, are enough
