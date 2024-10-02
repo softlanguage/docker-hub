4c66b01a36e4b74b4f681232f822b96366b432eb
softlang-net <bug.fyi@outlook.com>
2024-10-02 21:36:46 +0800

    npm Dockerfile to build both pc and mp

---
+++ docker/buildx/npm.make.sh
@@ -0,0 +1,10 @@
+set -e
+#export DOCKER_CONTEXT=pod01.dev
+docker rm -f frontend
+docker pull docker.io/node:18-slim
+docker pull docker.io/nginx:1.14-alpine
+
+docker build -t frontend:v1 -f vue.Dockerfile .
+NET_PORT="-p9000:5000 --network bridge --rm -d"
+printf "docker run --name frontend.lan -h frontend.lan ${NET_PORT} frontend:v1" \
+    |xargs -t -r -I{} sh -ec {}
