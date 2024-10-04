11c71c90882ab27bab6c9e710d9903effa31f6ac
softlang-net <bug.fyi@outlook.com>
2024-10-04 12:13:49 +0800

    fix bug of the variable app_name

--- openjdk/java17vue/crmls-ai/make.sh
+++ openjdk/java17vue/crmls-ai/make.sh
@@ -14,13 +14,13 @@ printf "docker run -d --rm --name $app_name -h $app_name.lan ${NET_PORT} $app_na
 # tar tvf java17vue.tar.gz
 
 app_name=api-java17
-docker rm -f $app_java
+docker rm -f $app_name
 docker pull -q docker.io/eclipse-temurin:17-jre-focal
 docker pull -q docker.io/maven:3.8.5-eclipse-temurin-17
 
-docker build -t $app_java:v1 -f Dockerfile . #--progress=plain
+docker build -t $app_name:v1 -f Dockerfile . #--progress=plain
 NET_PORT="-p9001:5000 --network bridge"
-printf "docker run -d --rm --name $app_java -h $app_java.lan ${NET_PORT} $app_java:v1" \
+printf "docker run -d --rm --name $app_name -h $app_name.lan ${NET_PORT} $app_name:v1" \
     |xargs -t -r -I{} sh -ec {}
 
 # tar czf vue-java.tar.gz --exclude='crmls-ai/.git/*' crmls-ai/
