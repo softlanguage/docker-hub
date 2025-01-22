3129e17eb25a44a8b0b7ce89bf2be80d51b52028
softlang-net <bug.fyi@outlook.com>
2025-01-22 18:00:28 +0800

    support cleanup

--- database/postgresql/_recovery/archive_restore_wal.sh
+++ database/postgresql/_recovery/archive_restore_wal.sh
@@ -10,11 +10,13 @@ dir_archived=/var/lib/postgresql/backup/archivedWAL
 # 1=$p,WAL_location, 2=%f,wal_leafname_name
 wal_location=$1
 wal_leafname=$2
+arg_cleanup=$4
 # dash-shell get 10bytes, max=15bytes, not support ${str:-15}
 hex_leafname=$(expr substr "$wal_leafname" $(expr length "$wal_leafname" - 9) 10)
 # remainder for wal_leafname: [10~999]
 remainder_base=5
-dir_rem=$(printf "d%03d\n" "$((0x${hex_leafname} % $remainder_base + 1))")
+remainder_value="$((0x$hex_leafname % $remainder_base + 1))"
+dir_rem=$(printf "d%03d" "$remainder_value")
 # location of archived WAL
 #printf "
 ## $(date +%F\ %T)
@@ -25,15 +27,28 @@ dir_rem=$(printf "d%03d\n" "$((0x${hex_leafname} % $remainder_base + 1))")
 #dir_rem=$dir_rem
 #" >> $dir_archived/debug.log
 
+# Create the directory if it doesn't exist
+if [ ! -d "$dir_archived/$dir_rem" ]; then
+  mkdir -p "$dir_archived/$dir_rem"
+fi
+
 do_archive(){
-    mkdir -p $dir_archived/$dir_rem
-    #cp -fa $wal_location $dir_archived/$dir_rem/$wal_leafname
-    zstd -c $wal_location > $dir_archived/$dir_rem/$wal_leafname.zst
+  #cp -fa $wal_location $dir_archived/$dir_rem/$wal_leafname
+  zstd -c $wal_location > $dir_archived/$dir_rem/$wal_leafname.zst
+  # zstd has better performance than gzip
+
+  # $4=cleanup
+  # archive_command='archive_restore_wal.sh %p %f do_archive cleanup'
+  if [ "$arg_cleanup" = "cleanup" ] && [ "$remainder_value" = "$remainder_base" ]; then
+    echo "$(date +%F\ %T) -- cleanup"
+    find $dir_archived -maxdepth 2 -name '*.zst' -type f -mtime +3 -print -exec rm -rf {} \;
+    # 3days ago: -mtime +3 , 180mins ago: -mmin +180
+  fi
 }
 
 do_restore(){
-    #cp $dir_archived/$dir_rem/$wal_leafname $wal_location
-    zstd -c -d $dir_archived/$dir_rem/$wal_leafname.zst > $wal_location
+  #cp $dir_archived/$dir_rem/$wal_leafname $wal_location
+  zstd -c -d $dir_archived/$dir_rem/$wal_leafname.zst > $wal_location
 }
 
 # call do_archive/do_restore
