cdf9f5d47d4558ed459b3c8c6856a141d685d527
softlang.vip <echo@softlang.vip>
2025-06-30 13:30:15 +0800

    Fix indentation in docker-io.sh and reorder Gitea image copies for clarity

--- gcrane/docker-io.sh
+++ gcrane/docker-io.sh
@@ -4,5 +4,5 @@ set -e
 # mysql
 crane copy docker.io/mysql:lts docker.io/softlang/mysql:8.4-lts
 crane copy docker.io/gitea/gitea:1.21.11 docker.io/softlang/gitea:1.21.11
-
-
+crane copy docker.io/gitea/gitea:1.22.6 docker.io/softlang/gitea:1.22.6
+crane copy docker.io/gitea/gitea:1.23.8 docker.io/softlang/gitea:1.23.8
